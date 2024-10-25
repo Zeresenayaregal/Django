8885f9d9531fa1184fcc5d068af340a4d3ccca39
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-10-25 20:50:10 +0300

    function for profile added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterationForm
 
 def register(request):
@@ -7,9 +8,13 @@ def register(request):
         form = UserRegisterationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'Your Account has been created!')
             return redirect('login')
     else:
         form = UserRegisterationForm()
     return render(request, 'users/register.html', {'form':form})
+
+
+@login_required(login_url='/login/')
+def profile(request):
+    return render(request, 'users/profile.html') 
