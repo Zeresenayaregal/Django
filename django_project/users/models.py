4f7be5597620749661496ad5b0dde37ae4c4e462
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-10-30 09:57:08 +0300

    default pic modified

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="default.jpg", upload_to='profile_pics')
+    image = models.ImageField(default="OIP.jfif", upload_to='profile_pics')
 
     def __str__(self):
         return super().__str__()  # or f"{self.user.username}"
