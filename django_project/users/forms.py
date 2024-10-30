0865853fbaa3eac8a3fb4bd396e1cdee106d78a6
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-10-31 02:21:48 +0300

    locally done

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 class UserRegisterationForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,14 @@ class UserRegisterationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="OIP.jfif", upload_to='profile_pics')
 
     def __str__(self):
         return super().__str__()  # or f"{self.user.username}"
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+         
+        if img.height > 300 and img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
