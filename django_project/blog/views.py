bf0df108df0551f4345d99577abf476d8e847ab3
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-11-06 21:39:23 +0300

    list CBV added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -8,6 +9,12 @@ def home(request):
         'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # The default format for the direction of the funtion is the following '<app>/<modelNmae>_<viewtype>.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
