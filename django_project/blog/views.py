74ad248d4209f6ef9fa2228d7053e6640d5fcd8d
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-11-09 21:21:30 +0300

    detail and create cbv

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -16,5 +17,19 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-created_at']
 
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    
+
+   
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
