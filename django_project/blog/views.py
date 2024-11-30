16d8c2e353383381565255bdd1f9113a3bc5a089
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-11-30 13:57:39 +0300

    unnecessary FBV removed

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -8,14 +8,6 @@ from django.views.generic import (ListView,
                                   DeleteView)
 from .models import Post
 
-
-
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-    }
-    return render(request, 'blog/home.html', context)
-
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # The default format for the direction of the funtion is the following '<app>/<modelNmae>_<viewtype>.html'
