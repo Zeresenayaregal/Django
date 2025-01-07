295f9182388af56f7fc1318609065e7a020f0c4b
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2025-01-07 22:16:57 +0300

    settings changed for deployment

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mc63q$9(t!e*qxrri#lw9rz^3y^ngvg7899)unc3a5u#*+2c*m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
